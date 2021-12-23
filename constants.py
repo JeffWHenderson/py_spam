0632153971de8c66ad0febdd8d784e033534ee61
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2021-12-22 22:06:46 -0500

    oops

--- constants.py
+++ constants.py
@@ -6,6 +6,6 @@ winnerName = "winner"  # <------ add winner name so they will be excluded from e
 # add shortcode (CW9Fh_As_1g is the short code for https://www.instagram.com/p/CW9Fh_As_1g/)
 post_short_code = "CXyvJYslp3I"
 
-message = """This message can have links and stuff... I don't want
+message = """This message can have links and stuff...
 the three quotes means you can put it on multiple lines
 every new line will also be a new line in the DM"""
