6935b97ca9b472680432d9f2b9d98ef257d0d799
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2021-12-22 22:04:40 -0500

    making sharable.

--- constants.py
+++ constants.py
@@ -6,5 +6,6 @@ winnerName = "winner"  # <------ add winner name so they will be excluded from e
 # add shortcode (CW9Fh_As_1g is the short code for https://www.instagram.com/p/CW9Fh_As_1g/)
 post_short_code = "CXyvJYslp3I"
 
-message = """Thanks for entering this week's ticket sweepstakes. Unfortunately you were not selected as this week's winner, but there's still a chance for you to see your Bengals this Sunday against the Ravens. The perfect last second holiday gift. You can purchase tickets with NO FEES using this link >>> https://go.bengals.com/30USGiO
-For questions, call the Bengals Ticket Hotline at (513) 621-8383. WHO DEY!"""
+message = """This message can have links and stuff... I don't want
+the three quotes means you can put it on multiple lines
+every new line will also be a new line in the DM"""
