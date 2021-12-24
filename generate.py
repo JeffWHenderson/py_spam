57d5e52cfa39715fbfdea513c95baecc00ad006d
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2021-12-23 22:36:01 -0500

    update the generate script to produce 4 files.

--- generate.py
+++ generate.py
@@ -1,4 +1,6 @@
+import math
 import instaloader
+
 from constants import username, user_secret, winnerName, post_short_code
 
 L = instaloader.Instaloader()
@@ -7,22 +9,29 @@ L.login(username, user_secret)
 def get_all_users_that_commented():
     post = instaloader.Post.from_shortcode(L.context, post_short_code)
 
-    commenters = []
+    commenters_array = []
 
-    i = 0
     for comment in post.get_comments():
         instagram_user = comment.owner.username
-        if instagram_user not in commenters:
+        if instagram_user not in commenters_array:
             if instagram_user != winnerName:
-                commenters.append(instagram_user)
-        i += 1
+                commenters_array.append(instagram_user)
 
-        if i == 50:
-            return commenters
-            break
+    return commenters_array
 
-    return commenters
+
+def split_array_into_text_files(num, commenters):
+    number_of_users_per_file = len(commenters) / num
+    index = 0
+
+    for x in range(num):  # run a loop num times
+        f = open("usernames_" + str(x + 1) + ".txt", "x")
+
+        for y in range(0, math.floor(number_of_users_per_file)):
+            f.write(commenters[index] + "\n")
+            index = index + 1
 
 
 if __name__ == '__main__':
-    get_all_users_that_commented()
+    commenters = get_all_users_that_commented()
+    split_array_into_text_files(4, commenters)
